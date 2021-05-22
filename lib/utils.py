74cd528483fa378bd098f0c554c2d282a6be4d55
alladin <limited.tulgaa@gmail.com>
2021-05-22 17:27:20 +0800

    add

--- lib/utils.py
+++ lib/utils.py
@@ -1,11 +1,12 @@
 import os
 from lib.params import SAVE_PATH
+from lib.params import LABEL_LIST
 
 
 def get_index():
     index = 0
     while True:
-        if not os.path.isdir(f'{SAVE_PATH}/2_1/{index}'):
+        if not os.path.isdir(f'{SAVE_PATH}/{LABEL_LIST[0]}/{index}'):
             break
         else:
             index += 1
